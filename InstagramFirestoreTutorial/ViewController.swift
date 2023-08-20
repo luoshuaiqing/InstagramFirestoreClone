ce30658f96e53987cea62f330c1e245b4bb11484
sluo <sluo@snapchat.com>
2023-08-20 15:39:54 -0700

    Initial Commit

---
+++ InstagramFirestoreTutorial/ViewController.swift
@@ -0,0 +1,14 @@
+// Copyright © 2023 Snap, Inc. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
