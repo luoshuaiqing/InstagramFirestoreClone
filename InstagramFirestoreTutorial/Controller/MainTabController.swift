138487faab30c9768d93ac958724c411dd07f964
sluo <sluo@snapchat.com>
2023-08-20 15:50:09 -0700

    update

---
+++ InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -0,0 +1,11 @@
+// Copyright © 2023 Snap, Inc. All rights reserved.
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
