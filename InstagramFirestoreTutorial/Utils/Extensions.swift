d08799015e9fa7c62da2a84fb76e33d5375791a2
sluo <sluo@snapchat.com>
2023-08-21 23:48:13 -0700

    update

--- InstagramFirestoreTutorial/Utils/Extensions.swift
+++ InstagramFirestoreTutorial/Utils/Extensions.swift
@@ -2,6 +2,18 @@
 
 import UIKit
 
+extension UIButton {
+    func attributedTitle(firstPart: String, secondPart: String) {
+        let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.systemFont(ofSize: 16)]
+        let attributedTitle = NSMutableAttributedString(string: "\(firstPart) ", attributes: atts)
+        
+        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.boldSystemFont(ofSize: 16)]
+        attributedTitle.append(NSAttributedString(string: secondPart, attributes: boldAtts))
+        
+        setAttributedTitle(attributedTitle, for: .normal)
+    }
+}
+
 extension UIView {
     func anchor(top: NSLayoutYAxisAnchor? = nil,
                 left: NSLayoutXAxisAnchor? = nil,
