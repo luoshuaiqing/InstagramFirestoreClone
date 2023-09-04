5fda73696b72a10e8b38483c72fdabf9c5d7ffd0
sluo <sluo@snapchat.com>
2023-09-04 11:41:20 -0700

    update

--- InstagramFirestoreTutorial/View/UserCell.swift
+++ InstagramFirestoreTutorial/View/UserCell.swift
@@ -6,10 +6,9 @@ class UserCell: UITableViewCell {
     
     // MARK: - Properties
     
-    var user: User? {
+    var viewModel: UserCellViewModel? {
         didSet {
-            usernameLabel.text = user?.username
-            fullnameLabel.text = user?.fullname
+            configure()
         }
     }
     
@@ -59,5 +58,15 @@ class UserCell: UITableViewCell {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+    
+    func configure() {
+        guard let viewModel = viewModel else { return }
+        
+        profileImageView.sd_setImage(with: viewModel.profileImageUrl)
+        usernameLabel.text = viewModel.username
+        fullnameLabel.text = viewModel.fullname
     }
 }
---
+++ InstagramFirestoreTutorial/ViewModel/UserCellViewModel.swift
@@ -0,0 +1,23 @@
+// Copyright © 2023 Snap, Inc. All rights reserved.
+
+import Foundation
+
+struct UserCellViewModel {
+    private let user: User
+    
+    var profileImageUrl: URL? {
+        return URL(string: user.profileImageUrl)
+    }
+    
+    var username: String {
+        return user.username
+    }
+    
+    var fullname: String {
+        return user.fullname
+    }
+    
+    init(user: User) {
+        self.user = user
+    }
+}
