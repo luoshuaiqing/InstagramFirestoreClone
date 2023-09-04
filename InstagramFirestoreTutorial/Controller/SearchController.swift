68014e144bd7a7b0b2e1ea583516c4a65c53bb3a
sluo <sluo@snapchat.com>
2023-09-04 11:42:01 -0700

    update

--- InstagramFirestoreTutorial/Controller/SearchController.swift
+++ InstagramFirestoreTutorial/Controller/SearchController.swift
@@ -49,7 +49,7 @@ extension SearchController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
-        cell.user = users[indexPath.row]
+        cell.viewModel = UserCellViewModel(user: users[indexPath.row])
         return cell
     }
 }
