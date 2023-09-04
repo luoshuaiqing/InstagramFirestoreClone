0faedaec4d86bf3f64c53c79cfccfc845bda3897
sluo <sluo@snapchat.com>
2023-09-04 15:50:59 -0700

    update

--- InstagramFirestoreTutorial/View/Profile/ProfileHeader.swift
+++ InstagramFirestoreTutorial/View/Profile/ProfileHeader.swift
@@ -49,7 +49,6 @@ class ProfileHeader: UICollectionReusableView {
         let label = UILabel()
         label.numberOfLines = 0
         label.textAlignment = .center
-        label.attributedText = attributedStatText(value: 5, label: "posts")
         return label
     }()
     
@@ -57,7 +56,6 @@ class ProfileHeader: UICollectionReusableView {
         let label = UILabel()
         label.numberOfLines = 0
         label.textAlignment = .center
-        label.attributedText = attributedStatText(value: 2, label: "followers")
         return label
     }()
     
@@ -65,7 +63,6 @@ class ProfileHeader: UICollectionReusableView {
         let label = UILabel()
         label.numberOfLines = 0
         label.textAlignment = .center
-        label.attributedText = attributedStatText(value: 1, label: "following")
         return label
     }()
     
@@ -149,14 +146,13 @@ class ProfileHeader: UICollectionReusableView {
         
         nameLabel.text = viewModel.fullname
         profileImageView.sd_setImage(with: viewModel.profileImageUrl)
+        
         editProfileFollowButton.setTitle(viewModel.followButtonText, for: .normal)
         editProfileFollowButton.setTitleColor(viewModel.followButtonTextColor, for: .normal)
         editProfileFollowButton.backgroundColor = viewModel.followButtonBackgroundColor
-    }
-    
-    func attributedStatText(value: Int, label: String) -> NSAttributedString {
-        let attributedText = NSMutableAttributedString(string: "\(value)\n", attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
-        attributedText.append(NSAttributedString(string: label, attributes: [.font: UIFont.systemFont(ofSize: 14), .foregroundColor: UIColor.lightGray]))
-        return attributedText
+        
+        postsLabel.attributedText = viewModel.numberOfPostsText
+        followersLabel.attributedText = viewModel.numberOfFollowersText
+        followingsLabel.attributedText = viewModel.numberOfFollowingsText
     }
 }
--- InstagramFirestoreTutorial/ViewModel/ProfileHeaderViewModel.swift
+++ InstagramFirestoreTutorial/ViewModel/ProfileHeaderViewModel.swift
@@ -31,7 +31,25 @@ struct ProfileHeaderViewModel {
         return user.isCurrentUser ? .black : .white
     }
     
+    var numberOfFollowersText: NSAttributedString {
+        return attributedStatText(value: user.stats.followers, label: "followers")
+    }
+    
+    var numberOfFollowingsText: NSAttributedString {
+        return attributedStatText(value: user.stats.followings, label: "followings")
+    }
+    
+    var numberOfPostsText: NSAttributedString {
+        return attributedStatText(value: 0, label: "posts")
+    }
+    
     init(user: User) {
         self.user = user
+    }
+    
+    private func attributedStatText(value: Int, label: String) -> NSAttributedString {
+        let attributedText = NSMutableAttributedString(string: "\(value)\n", attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
+        attributedText.append(NSAttributedString(string: label, attributes: [.font: UIFont.systemFont(ofSize: 14), .foregroundColor: UIColor.lightGray]))
+        return attributedText
     }
 }
