7ee113b7495d92fb70e8130eedb333fcf2fa5932
sluo <sluo@snapchat.com>
2023-09-04 11:57:38 -0700

    update

--- InstagramFirestoreTutorial/Controller/SearchController.swift
+++ InstagramFirestoreTutorial/Controller/SearchController.swift
@@ -9,6 +9,7 @@ class SearchController: UITableViewController {
     // MARK: - Properties
     
     private var users = [User]()
+    private var filteredUsers = [User]()
     private let searchController = UISearchController(searchResultsController: nil)
     
     // MARK: - Lifecycle
@@ -80,7 +81,7 @@ extension SearchController {
 extension SearchController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
         guard let searchText = searchController.searchBar.text?.lowercased() else { return }
-        
+        filteredUsers = users.filter{ $0.fullname.contains(searchText) || $0.username.contains(searchText) }
         
     }
 }
