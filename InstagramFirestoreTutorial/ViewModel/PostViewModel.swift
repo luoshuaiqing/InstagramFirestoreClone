82b10cd0138a217f439fb4c05d98b63bcac497aa
sluo <sluo@snapchat.com>
2023-09-05 22:21:40 -0700

    update

--- InstagramFirestoreTutorial/ViewModel/PostViewModel.swift
+++ InstagramFirestoreTutorial/ViewModel/PostViewModel.swift
@@ -4,19 +4,19 @@ import Foundation
 
 struct PostViewModel {
     private let post: Post
-    
+
     var imageUrl: URL? {
         return URL(string: post.imageUrl)
     }
-    
+
     var caption: String {
         return post.caption
     }
-    
+
     var likes: Int {
         return post.likes
     }
-    
+
     init(post: Post) {
         self.post = post
     }
