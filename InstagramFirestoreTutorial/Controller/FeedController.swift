af861fee114e93aadc67470bdaa466dc6d7ab382
sluo <sluo@snapchat.com>
2023-09-06 08:38:00 -0700

    update

--- InstagramFirestoreTutorial/Controller/FeedController.swift
+++ InstagramFirestoreTutorial/Controller/FeedController.swift
@@ -77,7 +77,7 @@ extension FeedController {
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! FeedCell
-
+        cell.delegate = self
         if let post = post {
             cell.viewModel = PostViewModel(post: post)
         } else {
@@ -97,5 +97,14 @@ extension FeedController: UICollectionViewDelegateFlowLayout {
         height += 50
         height += 60
         return CGSize(width: width, height: height)
+    }
+}
+
+// MARK: - FeedCellDelegate
+
+extension FeedController: FeedCellDelegate {
+    func cell(_ cell: FeedCell, wantsToShowCommentsFor post: Post) {
+        let controller = CommentController(collectionViewLayout: UICollectionViewFlowLayout())
+        navigationController?.pushViewController(controller, animated: true)
     }
 }
