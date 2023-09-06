39330f9889b2b5ea025229cada67ad99c661340b
sluo <sluo@snapchat.com>
2023-09-06 08:08:18 -0700

    update

--- InstagramFirestoreTutorial/Controller/ProfileController.swift
+++ InstagramFirestoreTutorial/Controller/ProfileController.swift
@@ -6,23 +6,23 @@ private let cellIdentifier = "ProfileCell"
 private let headerIdentifier = "ProfileHeader"
 
 class ProfileController: UICollectionViewController {
-    
+
     // MARK: - Properties
-    
+
     private var user: User
     private var posts = [Post]()
-    
+
     // MARK: - Lifecycle
-    
+
     init(user: User) {
         self.user = user
         super.init(collectionViewLayout: UICollectionViewFlowLayout())
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionView()
@@ -30,39 +30,39 @@ class ProfileController: UICollectionViewController {
         fetchUserStats()
         fetchPosts()
     }
-    
+
     // MARK: - API
-    
+
     func fetchUserStats() {
         UserService.fetchUserStats(uid: user.uid) { stats in
             self.user.stats = stats
             self.collectionView.reloadData()
         }
     }
-    
+
     func fetchUserFollowStatus() {
         UserService.fetchUserFollowStatus(uid: user.uid) { isFollowed in
             self.user.isFollowed = isFollowed
             self.collectionView.reloadData()
         }
     }
-    
+
     func fetchPosts() {
         PostService.fetchPosts(forUser: user.uid) { posts in
             self.posts = posts
             self.collectionView.reloadData()
         }
     }
-    
+
     // MARK: - Helpers
-    
+
     func configureCollectionView() {
         navigationItem.title = user.username
         collectionView.backgroundColor = .white
         collectionView.register(ProfileCell.self, forCellWithReuseIdentifier: cellIdentifier)
         collectionView.register(ProfileHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerIdentifier)
     }
-    
+
 }
 
 // MARK: - UICollectionViewDataSource
@@ -71,12 +71,12 @@ extension ProfileController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return posts.count
     }
-    
+
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! ProfileCell
         return cell
     }
-    
+
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerIdentifier, for: indexPath) as! ProfileHeader
         header.delegate = self
@@ -88,7 +88,7 @@ extension ProfileController {
 // MARK: - UICollectionViewDelegate
 
 extension ProfileController {
-    
+
 }
 
 // MARK: - UICollectionViewDelegateFlowLayout
@@ -97,16 +97,16 @@ extension ProfileController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 1
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 1
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = (view.frame.width - 2) / 3
         return CGSize(width: width, height: width)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 240)
     }
@@ -119,9 +119,9 @@ extension ProfileController: ProfileHeaderDelegate {
         if user.isCurrentUser {
             return
         }
-        
+
         guard let isFollowed = user.isFollowed else { return }
-        
+
         if isFollowed {
             UserService.unfollow(uid: user.uid) { error in
                 self.user.isFollowed = false
