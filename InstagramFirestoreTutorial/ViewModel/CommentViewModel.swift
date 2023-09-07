914f02dc6bb75b0e02b023906df106e2b46cc187
sluo <sluo@snapchat.com>
2023-09-06 22:28:32 -0700

    update

---
+++ InstagramFirestoreTutorial/ViewModel/CommentViewModel.swift
@@ -0,0 +1,24 @@
+// Copyright © 2023 Snap, Inc. All rights reserved.
+
+import Foundation
+
+struct CommentViewModel {
+    
+    private let comment: Comment
+    
+    var profileImageUrl: URL? {
+        return URL(string: comment.profileImageUrl)
+    }
+    
+    var username: String {
+        return comment.username
+    }
+    
+    var commentText: String {
+        return comment.comment
+    }
+    
+    init(comment: Comment) {
+        self.comment = comment
+    }
+}
