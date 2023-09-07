fef0190598df605f336bf064b5a455972cb73742
sluo <sluo@snapchat.com>
2023-09-06 22:33:26 -0700

    update

--- InstagramFirestoreTutorial/Controller/CommentController.swift
+++ InstagramFirestoreTutorial/Controller/CommentController.swift
@@ -82,7 +82,8 @@ extension CommentController {
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CommentCell
+        cell.viewModel = CommentViewModel(comment: comments[indexPath.row])
         return cell
     }
 }
