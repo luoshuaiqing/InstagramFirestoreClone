b352d3d53cb1e2a4ec7e59aaa8226e66582d2f61
sluo <sluo@snapchat.com>
2023-09-06 22:28:37 -0700

    update

--- InstagramFirestoreTutorial/ViewModel/CommentViewModel.swift
+++ InstagramFirestoreTutorial/ViewModel/CommentViewModel.swift
@@ -3,21 +3,21 @@
 import Foundation
 
 struct CommentViewModel {
-    
+
     private let comment: Comment
-    
+
     var profileImageUrl: URL? {
         return URL(string: comment.profileImageUrl)
     }
-    
+
     var username: String {
         return comment.username
     }
-    
+
     var commentText: String {
         return comment.comment
     }
-    
+
     init(comment: Comment) {
         self.comment = comment
     }
